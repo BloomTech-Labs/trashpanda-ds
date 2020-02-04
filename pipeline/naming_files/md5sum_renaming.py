307409c37704b1a69abf9da83c2d1cc06f00c023
veramendes <veramendes@users.noreply.github.com>
2020-02-03 22:03:57 -0800

    adding comments and organizing the .py file

--- pipeline/naming_files/md5sum_renaming.py
+++ pipeline/naming_files/md5sum_renaming.py
@@ -26,21 +26,25 @@ def hash_file(fpath):
     return hashlib.md5(file_as_bytes(open(fpath, 'rb'))).hexdigest()
 
 
+# get all the file_images locations from the images_to_rename folder
 file_images = os.listdir('./images_to_rename')
 fpaths = [f'./images_to_rename/{file_image}' for file_image in file_images]
 
-dsts = []
-for fname in fpaths:
+
+dsts = []   # empty list
+for fname in fpaths:   
+    # separating the directory path (head) from the file (tail)
     head, tail = os.path.split(fname)
-    print(os.path.split(fname))
+    # separating the name of the file from its extension
     name, ext = os.path.splitext(tail)
-    print(os.path.splitext(tail))
+    # getting md5sum for image file
     hash = hash_file(fname)
-    print(hash_file(fname))
+    # joining file hash with file extension
     dst = hash + ext
+    #checking for duplicate files
     if dst not in dsts:
         dsts.append(dst)
-        print(tail + " --> " + dst)
-        os.rename(fname, head + "/" + dst)
+        # print(tail + " --> " + dst)
+        os.rename(fname, head + "/" + dst)    # rename file as file hash + ext 
     else: 
-        os.remove(fname)
+        os.remove(fname)                      # remove the duplicates
