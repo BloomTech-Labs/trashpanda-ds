62eea694efed178097b93d7cf6a7679789a3b488
Trevor Clack <tclack@aol.com>
2020-02-29 00:04:53 -0800

    modify helper shell scripts, improve README directions

--- sagemaker/step1.sh
+++ sagemaker/step1.sh
@@ -5,11 +5,11 @@ cd darknet; aws s3 sync s3://the-trash-panda dataset
 
 mv dataset/images/* dataset/ && rmdir dataset/images
 
-cp /home/ec2-user/SageMaker/trashpanda-ds/sagemaker/{manual_label_check.py,train_test_split.sh, yolo-obj.cfg} .
+cp /home/ec2-user/SageMaker/trashpanda-ds/sagemaker/{manual_label_check.py,train_test_split.sh,yolo-obj.cfg} .
 
-cp /home/ec2-user/SageMaker/trashpanda-ds/sagemaker/{obj.data,obj.names,yolo-obj.cfg} darknet/data && cp darknet/data/obj.names classes.txt
+cp /home/ec2-user/SageMaker/trashpanda-ds/sagemaker/{obj.data,obj.names,yolo-obj.cfg} data && cp data/obj.names classes.txt
 
-train_test_split.sh
+chmod +x train_test_split.sh && ./train_test_split.sh
 
-echo 'Make Changes to C code (can run make and see errors'
+echo 'Make Changes to C code (can run make command to see errors)'
 
